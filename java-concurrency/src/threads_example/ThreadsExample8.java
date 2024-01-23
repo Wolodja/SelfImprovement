5ec86b46807fc948225b4c9e28e4b9e45f12355b
Wolodja <wolodjawaskovych@i.ua>
2024-01-23 13:38:56 +0100

    Daemon and join

---
+++ java-concurrency/src/threads_example/ThreadsExample8.java
@@ -0,0 +1,26 @@
+package threads_example;
+
+public class ThreadsExample8 {
+
+    public static void main(String[] args) {
+        Runnable runnable = () -> {
+            while(true) {
+                sleep(1000);
+                System.out.println("Running");
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.setDaemon(true);
+        thread.start();
+        sleep(3100);
+    }
+
+    public static void sleep(long millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java-concurrency/src/threads_example/ThreadsExample9.java
@@ -0,0 +1,32 @@
+package threads_example;
+
+public class ThreadsExample9 {
+
+    public static void main(String[] args) {
+        Runnable runnable = () -> {
+            for(int i = 0; i<5; i++){
+                sleep(1000);
+                System.out.println("Running");
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.setDaemon(true);
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void sleep(long millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
