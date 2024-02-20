99a1df9fa9e420f6a2d29cb5c3ab2f654b7667f6
Wolodja <wolodjawaskovych@i.ua>
2024-02-20 08:21:06 +0100

    Race conditions example 3

---
+++ java-concurrency/src/race_conditions/RaceConditionsExample3.java
@@ -0,0 +1,36 @@
+package race_conditions;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RaceConditionsExample3 {
+
+
+    public static void main(String[] args) {
+        Map<String, String> sharedMap = new ConcurrentHashMap<>();
+
+        Thread thread1 = new Thread(getRunnable(sharedMap));
+        Thread thread2 = new Thread(getRunnable(sharedMap));
+
+        thread1.start();
+        thread2.start();
+    }
+
+    private static Runnable getRunnable(Map<String, String> sharedMap) {
+        return () -> {
+            for (int i = 0; i < 1_000_000; i++) {
+                synchronized (sharedMap) {
+                    if (sharedMap.containsKey("key")) {
+                        String val = sharedMap.remove("key");
+                        if (val == null) {
+                            System.out.println("Iteration " + i + ": Value for 'key' was null");
+                        }
+                    } else {
+                        sharedMap.put("key", "value");
+                    }
+                }
+            }
+        };
+    }
+
+}
