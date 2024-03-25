113daa38c6dbb5a5e9d8aa83860d4c6e56e35348
Wolodja <wolodjawaskovych@i.ua>
2024-03-25 08:48:47 +0100

    ExecutorService example

---
+++ java-concurrency/src/executor_service/ExecutorServiceExample.java
@@ -0,0 +1,24 @@
+package executor_service;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceExample {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        executorService.execute(newRunnable("Task 1"));
+        executorService.execute(newRunnable("Task 2"));
+        executorService.execute(newRunnable("Task 3"));
+
+        executorService.shutdown();
+    }
+
+    private static Runnable newRunnable(String msg){
+        return () -> {
+            String completeMsg = Thread.currentThread().getName() + ": " + msg;
+            System.out.println(completeMsg);
+        };
+    }
+}
